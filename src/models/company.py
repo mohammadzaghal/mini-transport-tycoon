5c519c7c98fc14d8752a91e9e9ccb23097580fc3
Mohammad Alzaghal <mo.alzaghal@gmail.com>
2026-03-22 20:58:25 +0100

    Created company class and spend and earn functions

--- src/models/company.py
+++ src/models/company.py
@@ -7,6 +7,7 @@ class Company:
     money: int
     total_income: int = 0
     total_expenses: int = 0
+    
 
     def spend(self, amount: int) -> bool:
         if amount > self.money:
@@ -22,4 +23,3 @@ class Company:
     @property
     def is_bankrupt(self) -> bool:
         return self.money < 0
-
