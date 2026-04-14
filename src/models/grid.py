853502edf9382cd9119301ff9dc801eae0597d73
Kumar Tiya <gl4d0s@inf.elte.hu>
2026-04-14 12:21:07 +0200

    is track buildable

--- src/models/grid.py
+++ src/models/grid.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from collections.abc import Iterable
 
 from src.enums import TileType
@@ -13,9 +14,9 @@ class Grid:
             [Tile(x=x, y=y) for x in range(width)]
             for y in range(height)
         ]
+        self.facilities: dict = {}
 
     def in_bounds(self, x: int, y: int) -> bool:
-        # inside or not
         return 0 <= x < self.width and 0 <= y < self.height
 
     def get_tile(self, x: int, y: int) -> Tile:
@@ -29,7 +30,6 @@ class Grid:
             tile.tile_type = tile_type
 
     def neighbors4(self, x: int, y: int) -> list:
-        #get neighbors
         neighbors = []
         for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
             tile = self.get_tile(x + dx, y + dy)
@@ -42,8 +42,17 @@ class Grid:
             yield from row
 
     def is_road_buildable(self, x: int, y: int) -> bool:
-        #if road can be built
         tile = self.get_tile(x, y)
         if tile is None:
             return False
+        if tile.is_city_road:
+            return False
         return tile.tile_type in {TileType.GRASS, TileType.FOREST}
+
+    def is_track_buildable(self, x: int, y: int) -> bool:
+        tile = self.get_tile(x, y)
+        if tile is None:
+            return False
+        if tile.is_entry_point:
+            return False
+        return tile.tile_type in {TileType.GRASS, TileType.FOREST, TileType.TRACK, TileType.ROAD}
